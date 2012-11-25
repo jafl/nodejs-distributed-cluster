0f4a13bb4bb1f44920df7ca56edc77c176c903a4
John Lindal <github@newplanetsoftware.com>
2012-11-25 15:03:08 -0800

    split out udp, in preparation for parallel tcp

--- test/udp.js
+++ test/udp.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-var mod_dcluster = require('../index.js');
+var mod_cluster = require('../index.js');
 
-var cluster = mod_dcluster.join(
+var cluster = mod_cluster.join(
 {
 	type: 'udp4'
 });
